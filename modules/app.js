ec7a0b7308c4e5d542930b9e82474dae834a34ec
Taiwo Ogunyemi <ogunyemitaiwojohn@gmail.com>
2025-12-24 21:07:24 +0000

    aysnc/await, modules and promise

---
+++ modules/app.js
@@ -0,0 +1,6 @@
+// import greet, {add as sum, multiply} from './maths.js'
+import * as MathUtility from './maths.js'
+console.log(MathUtility.greet("John"))
+console.log(MathUtility.add(2,4))
+
+console.log(MathUtility.multiply(4,5))
---
+++ modules/maths.js
@@ -0,0 +1,13 @@
+export function add(a,b){
+    return a + b;
+}
+
+export function multiply(a,b){
+    return a * b;
+}
+
+export function greet(name){
+    return `Hello, ${name}`;
+}
+// export default greet;
+// export {add, multiply};
