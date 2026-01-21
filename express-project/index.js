7684ba6dca7772c41bf252a140c5f4ee0efef9ef
Taiwo Ogunyemi <ogunyemitaiwojohn@gmail.com>
2026-01-21 20:23:12 +0000

    simple post request with file system

--- express-project/index.js
+++ express-project/index.js
@@ -1,5 +1,25 @@
 const express = require("express"); // importing express Module
+const fs = require("fs");
+
 const app = express(); // Creating an express application
+
+// This enables req.body for JSON
+app.use(express.json());
+
+// File Path
+const DATA_FILE = "./users.json";
+
+function readUsers(){
+    const data = fs.readFileSync(DATA_FILE, "utf8");
+    if(!data.trim()){
+        return [];
+    }
+    return JSON.parse(data);
+}
+
+function writeUsers(users){
+    fs.writeFileSync(DATA_FILE, JSON.stringify(users));
+}
 
 // Route
 app.get("/", (req, res) => {
@@ -10,23 +30,36 @@ app.get("/", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-    const users = [
-        {
-            id: 1,
-            name: "Henry",
-            age: 13
-        },
-        {
-            id: 2,
-            name: "John",
-            age: 30
-        },
-    ];
+    const users = readUsers();
     res.json({
         success: true,
         data: users
     });
 });
+
+app.post("/users", (req, res) => {
+    const users = readUsers();
+    const {name, age, gender} = req.body;
+    const date = new Date();
+    const newUser = {
+        id: Date.now(),
+        name: name,
+        age: age,
+        gender: gender,
+        dateAdded: date.toISOString()
+    }
+
+    users.push(newUser);
+    writeUsers(users);
+
+    res.status(201).json({
+        success: true,
+        data: newUser
+    });
+
+});
+
+
 
 
 // Start server
