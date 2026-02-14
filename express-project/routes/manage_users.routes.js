287181e8b8b278cfd3b0724025166d96e5513e53
Taiwo Ogunyemi <ogunyemitaiwojohn@gmail.com>
2026-02-14 13:44:35 +0000

    Authorization routes implementation

--- express-project/routes/manage_users.routes.js
+++ express-project/routes/manage_users.routes.js
@@ -2,14 +2,16 @@ import express from 'express';
 import { validateUser } from '../middlewares/validateUser.js';
 import * as userController from '../controllers/manage_user.controller.js'
 import authMiddleware from '../middlewares/auth.middleware.js';
+import authorize from '../middlewares/authorize.middleware.js';
 
 const router = express.Router();
+const ADMIN = "admin";
 
-router.get("/", authMiddleware, userController.getUsers);
-router.get("/:id", authMiddleware, userController.getUserById)
-router.post("/", authMiddleware, validateUser, userController.createUser);
-router.put("/:id", authMiddleware, userController.updateUser)
-router.patch("/user/:id", authMiddleware, userController.patchUser)
-router.delete("/:id", authMiddleware, userController.deleteUser)
+router.get("/", authMiddleware, authorize(ADMIN), userController.getUsers);
+router.get("/:id", authMiddleware, authorize(ADMIN), userController.getUserById)
+router.post("/", authMiddleware, authorize(ADMIN), validateUser, userController.createUser);
+router.put("/:id", authMiddleware, authorize(ADMIN), userController.updateUser)
+router.patch("/user/:id", authMiddleware, authorize(ADMIN), userController.patchUser)
+router.delete("/:id", authMiddleware, authorize(ADMIN), userController.deleteUser)
 
 export default router;
--- express-project/routes/user.routes.js
+++ express-project/routes/user.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import * as authUser from '../controllers/user.controller.js'
 import authMiddleware from '../middlewares/auth.middleware.js';
+import authorize from '../middlewares/authorize.middleware.js';
 
 const router = express.Router();
-
+const USER = "user";
 // Admin Routes
 
-router.get("/profile", authMiddleware, authUser.getMyProfile);
+router.get("/profile", authMiddleware, authorize(USER), authUser.getMyProfile);
 router.post("/register", authUser.registerUser);
 router.post("/login", authUser.login);
 router.post("/refresh", authUser.refresh);
